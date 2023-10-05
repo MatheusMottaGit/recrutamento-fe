c13695390ecd051f512c9252ea5e31e878966d17
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-05 20:04:49 -0300

    feat:building components and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { Search } from "lucide-react";
 import { Button } from "./components/ui/button";
-import { Card, CardDescription, CardTitle } from "./components/ui/card";
+import { Card, CardContent, CardDescription, CardTitle } from "./components/ui/card";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { FormEvent, useState } from "react";
-import { Avatar, AvatarImage } from "./components/ui/avatar";
+import SearchedUserCard from "./components/searched-user-card";
 
-interface User {
+export interface User {
   name: string
   avatar_url: string
   bio: string
   email: string
   followers: number
   following: number
+  repos_url: string
 }
 
 export function App() {
@@ -35,17 +36,9 @@ export function App() {
     console.log(data)
   }
 
-  const userInfos = [
-    {
-      email: user?.email,
-      followers: user?.followers,
-      following: user?.following
-    }
-  ]
-
   return (
     <div className="px-10 py-10 min-h-screen flex">
-      <main className="flex flex-1 items-center justify-center gap-20">
+      <main className="flex flex-col flex-1 items-center justify-center gap-8">
         <Card className="p-4 space-y-4 w-80">
           <div className="grid justify-center text-5xl">
             <GitHubLogoIcon className="text-4xl" />
@@ -72,26 +65,7 @@ export function App() {
           </form>
         </Card>
 
-        {/* {user && (
-          <Card className="p-4">
-            <div className="flex gap-3">
-              <Avatar>
-                <AvatarImage src={user.avatar_url} />
-              </Avatar>
-
-              <div className="space-y-2">
-                <CardTitle>{user.name}</CardTitle>
-                <CardDescription>
-                  {user.bio}
-                </CardDescription>
-              </div>
-            </div>
-
-            {userInfos.map(info )}
-          </Card>
-        )} */}
-
-
+        {user && <SearchedUserCard user={user} />}
       </main>
     </div>
   )
