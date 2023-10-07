1cdc31463404d0ac9e77d4d657602dfb13779fe4
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-07 12:38:44 -0300

    feat:setting fetch pattern

--- src/context/github-context.tsx
+++ src/context/github-context.tsx
@@ -6,6 +6,7 @@ interface Github {
   getUserData: (url: string) => Promise<void>
   repos: Repo[]
   getRepos: (url: string) => Promise<void>
+  getRepoDetails: (url: string) => Promise<void>
 }
 
 export const GithubContext = createContext({} as Github)
@@ -13,24 +14,39 @@ export const GithubContext = createContext({} as Github)
 export const GithubProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null)
   const [repos, setRepos] = useState<Repo[]>([])
+  const [repo, setRepo] = useState<Repo | null>(null)
+
+  async function fetchApi<T>(url: string): Promise<T> {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+  }
 
   async function getUserData(url: string) {
-    const response = await fetch(url)
-    const data: User = await response.json()
-
+    const data = await fetchApi<User | null>(url)
     setUser(data)
   }
 
   async function getRepos(url: string) {
-    const response = await fetch(url)
-    const data: Repo[] = await response.json()
-
-    console.log(data)
+    const data = await fetchApi<Repo[]>(url)
     setRepos(data)
   }
 
+  async function getRepoDetails(url: string) {
+    const data = await fetchApi<Repo | null>(url)
+    setRepo(data)
+  }
+
   return (
-    <GithubContext.Provider value={{ repos, getRepos, getUserData, user }}>
+    <GithubContext.Provider value={{
+      repos,
+      getRepos,
+      getUserData,
+      user,
+      getRepoDetails
+    }}
+    >
       {children}
     </GithubContext.Provider>
   )
